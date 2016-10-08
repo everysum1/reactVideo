8d17de53fd8a85896d2f316b1828a5e419683d43
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 16:29:31 -0700

    Add composite component and refactor

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,23 @@
 var div = React.DOM.div
 var h1 = React.DOM.div
 
+// COMPOSITE COMPONENT
+var myTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, 'You can do it!')
+      )
+    )
+  }
+})
+
 var MyFirstComponent = (
   div(null, 
-    h1(null, "You can do it!")
+    React.createElement(myTitle, null),
+    React.createElement(myTitle, null),
+    React.createElement(myTitle, null),
+    React.createElement(myTitle, null)
   )
 )
 
