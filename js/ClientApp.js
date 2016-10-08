feaf2d0707beeb13b201600f108385e7a5e2f31f
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 16:48:23 -0700

    Add props handling for MyTitle component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,24 +2,24 @@ var div = React.DOM.div
 var h1 = React.DOM.div
 
 // COMPOSITE COMPONENT
-var myTitle = React.createClass({
+var MyTitle = React.createClass({
   render () {
     return (
       div(null,
-        h1(null, 'You can do it!')
+        h1(null, this.props.title)
       )
     )
   }
 })
 
-var MyTitleFactory = React.createFactory(myTitle)
+var MyTitleFactory = React.createFactory(MyTitle)
 ce = React.createElement
 
 var MyFirstComponent = (
   div(null, 
-    MyTitleFactory(null),
-    React.createElement(myTitle, null),
-    ce(myTitle, null)
+    MyTitleFactory({title: 'Props are great!'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!'}),
+    ce(MyTitle, {title: 'Props are the best!'})
   )
 )
 
