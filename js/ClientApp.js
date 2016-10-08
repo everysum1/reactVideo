6f5fad201181320a57718b5b9ba4ea50c98991b2
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 16:51:45 -0700

    Add notes

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,7 +3,10 @@ var h1 = React.DOM.div
 
 // COMPOSITE COMPONENT
 var MyTitle = React.createClass({
+  // every component must have a render method
   render () {
+    // every render method must return a component
+    // must be a pure function
     return (
       div(null,
         h1(null, this.props.title)
