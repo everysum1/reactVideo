c427b071de7142d3a7c4179a6da2e8357132cc00
Izzy Matos <israeldmatos@gmail.com>
2016-10-09 00:50:41 -0700

    Add splat operator to showcard component in search rendering function

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
 const Search = () => (
   <div className="container">
-    {data.shows.map((show) => (
-      <h3>{show.title}</h3>
-    ))}
+    <div className="shows">
+      {data.shows.map((show) => (
+        <ShowCard {...show} key={show.imdbID} />
+      ))}
+    </div>
   </div>
 )
 
