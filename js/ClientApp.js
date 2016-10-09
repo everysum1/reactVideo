abef93547372d3e5b0970747099d64119c3626c3
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 18:19:58 -0700

    Add extra element and final newline

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,13 +5,16 @@ var MyTitle = require('./MyTitle')
 
 var MyTitleFactory = React.createFactory(MyTitle)
 var ce = React.createElement
+var createEl = React.createElement
 
 var MyFirstComponent = (
   div(null,
     MyTitleFactory({title: React.DOM.span(null, 'Props are great!'), color: 'rebeccapurple'}),
     React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    createEl(MyTitle, {title: 'I\'m a random title', color: 'peru'}),
     ce(MyTitle, {title: 'Props are the best!', color: 'papayawhip'})
   )
 )
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+// MAIN FILE DOESN'T HAVE AN EXPORT COMMAND
