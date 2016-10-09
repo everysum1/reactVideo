a0d85e8e74ddfa6b5ff7d33936fdb49602517492
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 20:20:18 -0700

    Refactor component to es6 arrow function syntax

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,15 +2,13 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require('./MyTitle')
 
-var MyFirstComponent = function () {
-  return (
+var MyFirstComponent = () => (
     <div>
       <MyTitle title='There we were...' color='peru' />
       <MyTitle title="And this is where we're going" color='#f06d06' />
       <MyTitle title='Here we are!' color='rebeccapurple' />
     </div>
-  )
-}
+)
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
 // MAIN FILE DOESN'T HAVE AN EXPORT COMMAND
