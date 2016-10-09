45ba57b0ac837049f455742d71d5b25b803ff595
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 18:05:07 -0700

    Separate MyTitle class into its own file

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,21 +1,7 @@
 /* global React ReactDOM */
 
 var div = React.DOM.div
-var h1 = React.DOM.div
-
-// COMPOSITE COMPONENT
-var MyTitle = React.createClass({
-  // every component must have a render method
-  render () {
-    // every render method must return a component
-    // must be a pure function
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
+var MyTitle = require('./MyTitle')
 
 var MyTitleFactory = React.createFactory(MyTitle)
 var ce = React.createElement
---
+++ js/MyTitle.js
@@ -0,0 +1,18 @@
+var div = React.DOM.div
+var h1 = React.DOM.div
+
+// COMPOSITE COMPONENT
+var MyTitle = React.createClass({
+  // every component must have a render method
+  render () {
+    // every render method must return a component
+    // must be a pure function
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+
+module.exports = MyTitle
