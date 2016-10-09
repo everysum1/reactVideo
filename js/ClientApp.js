545d8eb30d430147feb52c2887bef4c833f26fbb
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 17:37:12 -0700

    Declare global variables, fix variable declaration

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,3 +1,5 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.div
 
@@ -16,11 +18,11 @@ var MyTitle = React.createClass({
 })
 
 var MyTitleFactory = React.createFactory(MyTitle)
-ce = React.createElement
+var ce = React.createElement
 
 var MyFirstComponent = (
-  div(null, 
-    MyTitleFactory({title: React.DOM.span(null,'Props are great!'), color: 'rebeccapurple'}),
+  div(null,
+    MyTitleFactory({title: React.DOM.span(null, 'Props are great!'), color: 'rebeccapurple'}),
     React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
     ce(MyTitle, {title: 'Props are the best!', color: 'papayawhip'})
   )
