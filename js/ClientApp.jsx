56c3b68c5f285b14140c382217dcf24c237c315b
Izzy Matos <israeldmatos@gmail.com>
2016-10-08 22:58:00 -0700

    Fix standard's spacing and new line errors

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const ReactRouter = require('react-router')
-const { Router, Route, hashHistory: hh } =  ReactRouter 
+const { Router, Route, hashHistory: hh } = ReactRouter
 
 // equivalent to line 5 pulling in ReactRouter
 // const Router = ReactRouter.Router
@@ -10,7 +10,7 @@ const { Router, Route, hashHistory: hh } =  ReactRouter
 // const hashHistory = ReactRouter.hashHistory
 
 const App = () => (
-  <Router history ={hh}>
+  <Router history={hh}>
     <Route path='/' component={Landing} />
   </Router>
 )
